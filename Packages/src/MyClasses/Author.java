4d988fdfb028bde8f883529503d8300afed1968d
Abba-Max <adjiabba10@gmail.com>
2025-11-28 05:17:43 +0100

    Feature: Preparation Protocol class updated

--- Packages/src/MyClasses/Author.java
+++ Packages/src/MyClasses/Author.java
@@ -6,4 +6,9 @@ public class Author extends Person {
     public Author(){
 
     }
+
+    public Author(int authorID, String name, String address, String contact, String dob ){ //dob for dateofbirth
+        super(name, address, contact,dob);
+        this.authorID = authorID;
+    }
 }
--- Packages/src/MyClasses/Prepprotocol.java
+++ Packages/src/MyClasses/Prepprotocol.java
@@ -4,8 +4,21 @@ import java.util.LinkedList;
 
 public class Prepprotocol implements  Conditions{
     public LinkedList<String> steps;
+    public LinkedList<Optcondition> stepConditions;
 
-    public Prepprotocol(){}
+    public Prepprotocol(){
+        steps = new LinkedList<>();
+        stepConditions = new LinkedList<>();
+    }
+
+    public void addStep(String desc, Optcondition cond){
+        steps.add(desc); //desc is for the description of the step of preparation
+        stepConditions.add(cond);
+    }
+
+
+
+
     public void Create(){
         return;
     }
