3517014a33149f0c4f5d2437500909bc86390158
Abba-Max <adjiabba10@gmail.com>
2025-11-28 05:08:07 +0100

    Feature: Drink class updated

--- Packages/src/MyClasses/Drink.java
+++ Packages/src/MyClasses/Drink.java
@@ -1,13 +1,33 @@
 package MyClasses;
 
+import java.util.LinkedList;
+
 public class Drink extends Item {
     public int drinkID;
-
+    public LinkedList<Ingredient> ingredients;
+    public Optcondition labCondition;
+    public LinkedList<String> standards;
+    public Prepprotocol prepprotocol;
+    public LinkedList<Author> authors;
+    public LinkedList<String> feedbacks;
+    public Conservecondition conservecondition;
+    public Trademarkinfo trademarkInfo;
+    public boolean vetoed;
+    public double averagePricePerKg;
     public Drink(){
 
     }
 
     public void updateDrinkInfo(){
 
+    }
+    public void addIngredient(Ingredient ingredient) {
+        if (ingredients == null) ingredients = new LinkedList<>();
+        ingredients.add(ingredient);
+    }
+
+    public void addFeedback(String feedback) {
+        if (feedbacks == null) feedbacks = new LinkedList<>();
+        feedbacks.add(feedback);
     }
 }
