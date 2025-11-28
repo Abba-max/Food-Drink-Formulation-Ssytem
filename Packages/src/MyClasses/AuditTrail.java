73bb58f34bd72fff4b3044b16960792099b02818
Abba-Max <adjiabba10@gmail.com>
2025-11-28 05:37:45 +0100

    Feature: AuditTrail and Veto classes created

---
+++ Packages/src/MyClasses/AuditTrail.java
@@ -0,0 +1,14 @@
+package MyClasses;
+
+import java.util.LinkedList;
+
+public class AuditTrail {
+    public LinkedList<String> records;
+
+    public AuditTrail(){
+        records = new LinkedList<>();
+    }
+    public void logAction(String user, String action){
+        records.add(user + "performed" + action);
+    }
+}
---
+++ Packages/src/MyClasses/Veto.java
@@ -0,0 +1,18 @@
+package MyClasses;
+
+import java.util.Date;
+
+public class Veto {
+    public boolean isVetoed;
+    public String reason;
+    public Date date;
+    public Person initiator;
+
+    public Veto(boolean isVetoed, String reason, Date date,Person initiator ){
+        this.isVetoed = isVetoed;
+        this.reason = reason;
+        this.date = date;
+        this.initiator = initiator;
+    }
+
+}
