e737ac9afd29ca4f7d5b6c0510ec3c952353d229
Abba-Max <adjiabba10@gmail.com>
2025-12-01 05:53:35 +0100

    Person class updated

--- Packages/src/MyClasses/Person.java
+++ Packages/src/MyClasses/Person.java
@@ -7,7 +7,56 @@ public class Person {
     private String contact;
     private String dateofbirth;
 
+    public int getPersonID() {
+        return personID;
+    }
 
+    public void setPersonID(int personID) {
+        this.personID = personID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getContact() {
+        return contact;
+    }
+
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
+
+    public String getDateofbirth() {
+        return dateofbirth;
+    }
+
+    public void setDateofbirth(String dateofbirth) {
+        this.dateofbirth = dateofbirth;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "personID=" + personID +
+                ", name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                ", contact='" + contact + '\'' +
+                ", dateofbirth='" + dateofbirth + '\'' +
+                '}';
+    }
 
 
 }
