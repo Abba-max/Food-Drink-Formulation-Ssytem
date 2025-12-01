0773b9b4933312c4be8342d7a28e4a1cf69b544c
Abba-Max <adjiabba10@gmail.com>
2025-12-01 05:51:26 +0100

    Quantity class updated

--- Packages/src/MyClasses/Quantity.java
+++ Packages/src/MyClasses/Quantity.java
@@ -1,5 +1,8 @@
 package MyClasses;
 
+import java.io.Serializable;
+import java.util.Objects;
+
 public class Quantity extends Ingredient {
     public double weight;
     public double volume;
@@ -8,8 +11,51 @@ public class Quantity extends Ingredient {
 
 
 
-    public  Quantity(double weight, double volume){
+    public Quantity() {}
+
+    public Quantity(double weight, double volume, double fraction, String unit) {
         this.weight = weight;
         this.volume = volume;
+        this.fraction = fraction;
+        this.unit = unit;
     }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public double getVolume() {
+        return volume;
+    }
+
+    public void setVolume(double volume) {
+        this.volume = volume;
+    }
+
+    public double getFraction() {
+        return fraction;
+    }
+
+    public void setFraction(double fraction) {
+        this.fraction = fraction;
+    }
+
+    public String getUnit() {
+        return unit;
+    }
+
+    public void setUnit(String unit) {
+        this.unit = unit;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Quantity{weight=%.2f, volume=%.2f, fraction=%.2f, unit='%s'}",
+                weight, volume, fraction, unit);
+    }
+
 }
